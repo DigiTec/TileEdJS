9599c0704a014cf1cea7e4e86bdb5f15c5a079fc
Justin Rogers <justrog@oculus.com>
2018-09-02 15:12:08 -0700

    Commit updated tests

--- __tests__/TMXImporter.spec.ts
+++ __tests__/TMXImporter.spec.ts
@@ -1,28 +1,28 @@
-import { TMXImporter } from "../src/TMXImporter";
+import {TMXImporter} from '../src/TMXImporter';
 import fs = require('fs');
 
-const basicLevel = fs.readFileSync("maps/Level1.tmx");
-const csvLevel = fs.readFileSync("maps/CSVCoded.tmx");
-const base64Level = fs.readFileSync("maps/Base64Coded.tmx");
+const basicLevel = fs.readFileSync('maps/Level1.tmx');
+const csvLevel = fs.readFileSync('maps/CSVCoded.tmx');
+const base64Level = fs.readFileSync('maps/Base64Coded.tmx');
 
 
-describe("Make sure a standard level can load.", () => {
-  test("Load a basic level using TMXImporter.loadFromString", () => {
+describe('Make sure a standard level can load.', () => {
+  test('Load a basic level using TMXImporter.loadFromString', () => {
     const importer = new TMXImporter();
     importer.loadFromString(basicLevel.toString());
   });
 
-  test("Load a CSV level using TMXImporter.loadFromString", () => {
+  test('Load a CSV level using TMXImporter.loadFromString', () => {
     const importer = new TMXImporter();
     importer.loadFromString(csvLevel.toString());
   });
 
-  test("Load a Base64 level using TMXImporter.loadFromString", () => {
+  test('Load a Base64 level using TMXImporter.loadFromString', () => {
     const importer = new TMXImporter();
     importer.loadFromString(base64Level.toString());
   });
 
-  test("Investigate Map", () => {
+  test('Investigate Map', () => {
     const tmxMap = new TMXImporter().loadFromString(basicLevel.toString());
     const tmxMap2 = new TMXImporter().loadFromString(csvLevel.toString());
     expect(tmxMap.layerCount).toEqual(2);
--- __tests__/XMLParserHelpers.spec.ts
+++ __tests__/XMLParserHelpers.spec.ts
@@ -1,10 +1,57 @@
-import { XmlParserHelpers } from "../src/XmlParserHelpers";
+import {XmlParserHelpers} from '../src/XmlParserHelpers';
 
 const parser = new DOMParser();
 
-describe("Required Attribute String Tests", () => {
-    test("A required string returns the right value.", () => {
-        const node = parser.parseFromString("<test existing='true'></test>", "text/xml").documentElement;
-        expect(XmlParserHelpers.requiredNodeValue(node, "existing")).toEqual("true");
-    });
+describe('Required Attribute String Tests', () => {
+  it('A required string returns the right value.', () => {
+    const node =
+        parser.parseFromString('<test existing=\'true\'></test>', 'text/xml')
+            .documentElement;
+    expect(XmlParserHelpers.requiredNodeValue(node, 'existing'))
+        .toEqual('true');
+    expect(XmlParserHelpers.requiredAttrValue(node.attributes, 'existing'))
+        .toEqual('true');
+  });
+
+  it('A required string throws if it does not exist.', () => {
+    const node =
+        parser.parseFromString('<test existing=\'true\'></test>', 'text/xml')
+            .documentElement;
+    expect(() => {
+      XmlParserHelpers.requiredNodeValue(node, 'nonexisting');
+    }).toThrow();
+    expect(() => {
+      XmlParserHelpers.requiredAttrValue(node.attributes, 'nonexisting');
+    }).toThrow();
+  });
+});
+
+describe('Required Attribute Number Tests', () => {
+  it('A required number returns the right value.', () => {
+    const node = parser.parseFromString('<test num=\'5\'></test>', 'text/xml')
+                     .documentElement;
+    expect(XmlParserHelpers.requiredNodeInteger(node, 'num')).toEqual(5);
+    expect(XmlParserHelpers.requiredAttrInteger(node.attributes, 'num'))
+        .toEqual(5);
+  })
+
+  it('A required number throws if it does not exist.', () => {
+    const node = parser.parseFromString('<test num=\'5\'></test>', 'text/xml')
+                     .documentElement;
+    expect(() => {
+      XmlParserHelpers.requiredNodeValue(node, 'missingnum');
+    }).toThrow();
+    expect(() => {
+      XmlParserHelpers.requiredAttrValue(node.attributes, 'missingnum');
+    }).toThrow();
+  });
+
+  it('A required number returns NaN if it fails to parse.', () => {
+    const node =
+        parser.parseFromString('<test num=\'notanum\'></test>', 'text/xml')
+            .documentElement;
+    expect(XmlParserHelpers.requiredNodeInteger(node, 'num')).toBeNaN();
+    expect(XmlParserHelpers.requiredAttrInteger(node.attributes, 'num'))
+        .toBeNaN();
+  });
 });
